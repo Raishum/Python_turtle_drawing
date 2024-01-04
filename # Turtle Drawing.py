c0dd06d8d42e3a86047674fad36bf539ffba21c2
Raishum <114391237+Raishum@users.noreply.github.com>
2024-01-04 18:07:48 +0000

    internal upload

---
+++ # Turtle Drawing.py
@@ -0,0 +1,18 @@
+# Turtle Drawing
+
+from turtle import*
+color("blue")
+shape("turtle")
+
+# Size of Pen
+speed(2)
+# Thikness of the line
+pensize(4)
+# Movemnet of the turtle
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
+right(90)
+forward(50)
